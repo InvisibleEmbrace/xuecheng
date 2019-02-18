3d4080bce7cd80f411e60251380a99c6b560a912
fjw <1281043316@qq.com>
2019-02-18 21:00:09 +0800

    first commit

---
+++ xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.xuecheng.manage_cms.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.CmsPageParam;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CmsPageRepositoryTest {
+
+    @Autowired
+    private CmsPageRepository cmsPageRepository;
+
+    //分页测试
+    @Test
+    public void testFindPage() {
+        int page = 0;//从0开始
+        int size = 10;//每页记录数
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        System.out.println(all);
+    }
+
+    //添加
+    @Test
+    public void testInsert() {
+        //定义实体类
+        CmsPage cmsPage = new CmsPage();
+        cmsPage.setSiteId("s01");
+        cmsPage.setTemplateId("t01");
+        cmsPage.setPageName("测试页面");
+        cmsPage.setPageCreateTime(new Date());
+        List<CmsPageParam> cmsPageParams = new ArrayList<>();
+        CmsPageParam cmsPageParam = new CmsPageParam();
+        cmsPageParam.setPageParamName("param1");
+        cmsPageParam.setPageParamValue("value1");
+        cmsPageParams.add(cmsPageParam);
+        cmsPage.setPageParams(cmsPageParams);
+        cmsPageRepository.save(cmsPage);
+        System.out.println(cmsPage);
+    }
+
+    //删除
+    @Test
+    public void testDelete() {
+        cmsPageRepository.deleteById("5c6aa188137b8a3dbc9691b6");
+    }
+
+    //修改
+    @Test
+    public void testUpdate() {
+        Optional<CmsPage> optional = cmsPageRepository.findById("5c6aa188137b8a3dbc9691b6");
+        if (optional.isPresent()) {
+            CmsPage cmsPage = optional.get();
+            cmsPage.setPageName("测试页面01");
+            cmsPageRepository.save(cmsPage);
+        }
+    }
+
+    @Test
+    public void testFindByPageName() {
+        CmsPage page = cmsPageRepository.findByPageName("测试页面01");
+        System.out.println(page);
+    }
+
+}
